ca5e9abe03653f229c8ec43dec0b9e3b5016a454
Changhyung <pck4949@naver.com>
2022-08-01 01:38:58 +0900

    :lipstick: 로그인 화면, 기본 UI 구성

---
+++ app/src/main/java/com/softsquared/gridge_test/android/instagram_challenge/src/fragment/sign_in/controller/SignInFragment.kt
@@ -0,0 +1,17 @@
+package com.softsquared.gridge_test.android.instagram_challenge.src.fragment.sign_in.controller
+
+import android.os.Bundle
+import android.view.View
+import com.softsquared.gridge_test.android.instagram_challenge.R
+import com.softsquared.gridge_test.android.instagram_challenge.config.BaseFragment
+import com.softsquared.gridge_test.android.instagram_challenge.databinding.FragmentSignInBinding
+
+class SignInFragment : BaseFragment<FragmentSignInBinding>(
+    FragmentSignInBinding::bind,
+    R.layout.fragment_sign_in) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
