c93f9b6bea9eeb7fc231d8a82946adf7eaddc0a1
Changhyung <pck4949@naver.com>
2022-07-31 16:22:15 +0900

    :wrench: 기본 템블릿 설정

---
+++ app/src/main/java/com/softsquared/gridge_test/android/instagram_challenge/config/BaseFragment.kt
@@ -0,0 +1,47 @@
+package com.softsquared.gridge_test.android.instagram_challenge.config
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+// Fragment의 기본을 작성, 뷰 바인딩 활용
+abstract class BaseFragment<B : ViewBinding>(
+    private val bind: (View) -> B,
+    @LayoutRes layoutResId: Int
+) : Fragment(layoutResId) {
+    private var _binding: B? = null
+
+    protected val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bind(super.onCreateView(inflater, container, savedInstanceState)!!)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    fun showCustomToast(message: String) {
+        Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
+    }
+
+    // 로딩 다이얼로그, 즉 로딩창을 띄워줌.
+    // 네트워크가 시작될 때 사용자가 무작정 기다리게 하지 않기 위해 작성.
+    // TODO: Add showLoadingDialog
+
+
+    // 띄워 놓은 로딩 다이얼로그를 없앰.
+    // TODO: Add dismissLoadingDialog
+}
---
+++ app/src/main/java/com/softsquared/gridge_test/android/instagram_challenge/src/main/controller/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.softsquared.gridge_test.android.instagram_challenge.src.main.controller
+
+import android.os.Bundle
+import com.softsquared.gridge_test.android.instagram_challenge.config.BaseActivity
+import com.softsquared.gridge_test.android.instagram_challenge.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
