eb8dffa7cd1a9872cb6c9b3a6789e12235286014
Changhyung <pck4949@naver.com>
2022-08-01 01:38:45 +0900

    :lipstick: 스플래시 화면, 기본 UI 구성

---
+++ app/src/main/java/com/softsquared/gridge_test/android/instagram_challenge/src/fragment/spash/controller/SplashFragment.kt
@@ -0,0 +1,19 @@
+package com.softsquared.gridge_test.android.instagram_challenge.src.fragment.spash.controller
+
+import android.os.Bundle
+import android.view.View
+import com.softsquared.gridge_test.android.instagram_challenge.R
+import com.softsquared.gridge_test.android.instagram_challenge.config.BaseFragment
+import com.softsquared.gridge_test.android.instagram_challenge.databinding.FragmentSplashBinding
+
+class SplashFragment : BaseFragment<FragmentSplashBinding>(
+    FragmentSplashBinding::bind,
+    R.layout.fragment_splash) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
